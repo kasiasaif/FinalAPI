74dddfa31d6bc11b0562659639516a3a905937ad
CodeMD96 <codemd96@gmail.com>
2022-01-18 15:25:13 +0000

    tournament db added with CRU of CRUD functionality

--- src/tournament/tournament.model.js
+++ src/tournament/tournament.model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const tournamentSchema = new mongoose.Schema({
-    tournamentname: {
+    tournamentName: {
         type: String,
         required: true,
         unique: true,
