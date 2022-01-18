437eb47e89d201b6ebe73bf2d7c9a74b72adf74f
kasiasaif <kasia.saif@gmail.com>
2022-01-18 12:09:53 +0000

    update&delete

--- src/user/user.routes.js
+++ src/user/user.routes.js
@@ -1,11 +1,24 @@
 const { Router } = require("express");
-const { addUser, logIn } = require("./user.controllers");
+const { addUser, 
+    logIn,
+    findOne,
+    findAll,
+    updateEmail,
+    updatePassword,
+    deleteUser 
+} = require("./user.controllers");
 const { hashing, tokenCheck } = require("../middleware");
 const userRouter = Router();
 
 userRouter.post("/user", hashing, addUser);
 userRouter.post("/login", logIn);
 userRouter.get("/user", tokenCheck, logIn);
+userRouter.get("/user/:username", findOne);
+userRouter.get("/users", findAll);
+userRouter.put("/user", updateEmail);
+userRouter.put("/user",updatePassword);
+userRouter.delete("/user/delete", deleteUser);
+
 
 module.exports = userRouter;
 
